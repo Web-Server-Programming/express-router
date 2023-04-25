fefe575eb44d6fdf1d6a1503b633f1b0c05d0eb5
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-04-25 11:00:26 +0530

    Add. New Router pageshome,contactUs,aboutUs added.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 
+const homePage = require('./router/home.js')
+const aboutUsPage = require('./router/aboutUs.js')
+const contactUsPage = require('./router/contactUs.js')
+
 app.get('/',(request,response)=>{
     response.send("This is home Page")
 })
+
+app.use('/home',homePage)
+app.use('/aboutUs',aboutUsPage)
+app.use('/contactUs',contactUsPage)
 
 app.listen(3500)
